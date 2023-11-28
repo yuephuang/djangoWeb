17edb0bcf682f99abca7aa7dc9c1e36c76f08990
huangyuepeng <1340557218@qq.com>
2023-11-28 23:00:56 +0800

    表格更改

--- blog/models.py
+++ blog/models.py
@@ -12,19 +12,19 @@ class Article(models.Model):
     """
     文章
     """
-    uuid = models.CharField(max_length=200)  # 散列值id
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # 文章作者
-    article_title = models.CharField(max_length=200)  # 文章标题
-    article_type = models.CharField(choices=TXT_TYPE, max_length=10)  # 文章类型
-    article_tag = models.CharField(max_length=200, null=True)  # 文章标签
-    article_url = models.CharField(max_length=200)  # 文章地址
-    created_date = models.DateTimeField(default=timezone.now)  # 上传时间
-    updated_date = models.DateTimeField(blank=True, null=True, default=timezone.now)  # 更新时间
-    del_flag = models.BooleanField(default=True)  # 是否删除
+    uuid = models.CharField(max_length=200)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article_title = models.CharField(max_length=200)
+    article_type = models.CharField(choices=TXT_TYPE, max_length=10)
+    article_tag = models.CharField(max_length=200, null=True)
+    article_url = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    del_flag = models.BooleanField(default=True)
 
     def publish(self):
         self.updated_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.article_title
--- blog/views.py
+++ blog/views.py
@@ -8,5 +8,5 @@ from blog.models import Article
 
 
 def post_list(request):
-    posts = Article.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    article = Article.objects.filter(updated_date__lte=timezone.now()).order_by('updated_date').first()
+    return render(request, 'blog/Article_base.html', {'article': article})
